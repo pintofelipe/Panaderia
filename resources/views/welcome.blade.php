499df61417f610248ff2c1544ad43e800459ba29
pintofelipe <afpintou@ufpso.edu.co>
2024-03-21 16:28:33 -0500

    add icono

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+    <link rel="icon" type="image/jpg" href="{{asset('favicon.ico')}}"/>
     <!-- Styles -->
     <style>
         /* ! tailwindcss v3.2.4 | MIT License | https://tailwindcss.com */
