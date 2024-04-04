ef305cb550e7e4bfd9b531b6eef04cc50247d9f7
pintofelipe <ufpintou@ufpso.edu.co>
2024-04-03 22:15:41 -0500

    add atributos a la tabla order

--- database/migrations/2024_03_20_191623_create_orders_table.php
+++ database/migrations/2024_03_20_191623_create_orders_table.php
@@ -12,6 +12,9 @@ return new class extends Migration {
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('date');
+            $table->string('order');
+            $table->decimal('total');
 
             $table->unsignedBigInteger('client_id');
             $table->foreign('client_id')
