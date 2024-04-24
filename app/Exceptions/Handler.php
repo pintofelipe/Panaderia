40beedeb36cea1d21b0affd6c5e8c81ff43fb0d6
afpintou@ufpso.edu.co <afpintou@ufpso.edu.co>
2024-04-24 14:28:12 -0500

    add archivos

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,9 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Exceptions\NotFoundHttpException;
+
+
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +29,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof NotFoundHttpException) {
+            return $exception->render($request);
+        }
+
+        return parent::render($request, $exception);
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+<div class="d-flex align-items-center justify-content-center bg-danger" style="background-image: url({{asset('backend/dist/img/espacio.jpg')}}) ; height:575px">
+  <section class="content">
+        <div class="error-page mt-5">
+          <h2 class="headline text-warning"> 404</h2>
+
+          <div class="error-content">
+            <h3><i class="fas fa-exclamation-triangle text-warning"></i class="text-lg"> Oops! Pagina no encontrada.</h3>
+
+            <p>
+               ¡Te hemos perdido! No sabemos que ha podido pasar.. <br>        
+               ¿Pero donde te has metido? Rapido vuelve con nosotros.
+            </p>
+            <div class="d-flex justify-content-center mr-5">
+              <a href="{{route('home')}}" class="btn btn-success">Regresar</a>
+            </div> 
+          </div>
+          <!-- /.error-content -->
+        </div>
+        <!-- /.error-page -->
+  </section>
+</div>
+
+
+@endsection
