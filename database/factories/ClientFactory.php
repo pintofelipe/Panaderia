972aeb9e04fa686ab83e8c2f807a5e493035e236
pintofelipe <afpintou@ufso.edu.co>
2024-05-28 16:46:36 -0500

    add photo clients

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -18,6 +18,7 @@ class ClientFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
+            'photo' => randomPhoto(),
             'document' => $this->faker->numberBetween(1234567890, 2134567890),
             'address' => $this->faker->address,
             'city' => $this->faker->city,
@@ -27,4 +28,9 @@ class ClientFactory extends Factory
             'status' => "1",
         ];
     }
+}
+
+function randomPhoto(): string
+{
+    return "dummyPhoto/" . rand(1, 17) . ".jpg";
 }
